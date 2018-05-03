6b9d4f78602e711cb3b8a09d61e7fcc0ec27dbcb
Daniel del Castillo <ddelcastilloperez@hotels.com>
2018-05-03 16:23:45 +0100

    [BDP-1234] More tests

--- shunting-yard-receiver/shunting-yard-receiver-kafka/src/main/java/com/hotels/shunting/yard/receiver/kafka/messaging/KafkaMessageReader.java
+++ shunting-yard-receiver/shunting-yard-receiver-kafka/src/main/java/com/hotels/shunting/yard/receiver/kafka/messaging/KafkaMessageReader.java
@@ -24,7 +24,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
@@ -32,6 +31,7 @@ import com.google.common.annotations.VisibleForTesting;
 
 import com.hotels.shunting.yard.common.event.SerializableListenerEvent;
 import com.hotels.shunting.yard.common.io.MetaStoreEventSerDe;
+import com.hotels.shunting.yard.common.io.SerDeException;
 import com.hotels.shunting.yard.common.messaging.MessageReader;
 
 public class KafkaMessageReader implements MessageReader {
@@ -86,11 +86,12 @@ public class KafkaMessageReader implements MessageReader {
     }
   }
 
-  private SerializableListenerEvent eventPayLoad(ConsumerRecord<Long, byte[]> record) {
+  private SerializableListenerEvent eventPayLoad(ConsumerRecord<Long, byte[]> message) {
     try {
-      return eventSerDe.unmarshall(record.value());
-    } catch (MetaException e) {
-      throw new RuntimeException("Unable to unmarshall event", e);
+      return eventSerDe.unmarshall(message.value());
+    } catch (Exception e) {
+      // TODO this may be removed when we get rid off checked exceptions in the SerDe contract
+      throw new SerDeException("Unable to unmarshall event", e);
     }
   }
 
